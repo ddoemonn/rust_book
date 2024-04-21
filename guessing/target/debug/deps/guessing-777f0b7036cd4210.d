09e0f08562df39d5ed9ee6521a6f330d5c6a8652
ddoemonn <ozergokalpsezer@gmail.com>
2024-04-21 17:18:58 +0300

    complete

--- guessing/target/debug/deps/guessing-777f0b7036cd4210.d
+++
@@ -1,5 +0,0 @@
-/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/guessing-777f0b7036cd4210: src/main.rs
-
-/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/guessing-777f0b7036cd4210.d: src/main.rs
-
-src/main.rs:
--- guessing/target/debug/deps/guessing-ed037bcb71e959ec.d
+++
@@ -1,5 +0,0 @@
-/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/libguessing-ed037bcb71e959ec.rmeta: src/main.rs
-
-/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/guessing-ed037bcb71e959ec.d: src/main.rs
-
-src/main.rs:
