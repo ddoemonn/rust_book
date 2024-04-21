abe49e755b0a2c2b91b11a3e61bab0d5e6ee795f
ddoemonn <ozergokalpsezer@gmail.com>
2024-04-21 16:45:55 +0300

    REMOVE: DS_Store

---
+++ guessing/target/debug/deps/guessing-06fb5ccb262e9df4.d
@@ -0,0 +1,5 @@
+/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/libguessing-06fb5ccb262e9df4.rmeta: src/main.rs
+
+/Users/ozergokalpsezer/Desktop/rust_book/guessing/target/debug/deps/guessing-06fb5ccb262e9df4.d: src/main.rs
+
+src/main.rs:
